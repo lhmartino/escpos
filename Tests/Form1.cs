3a9298dbaa3cddad57f91d5a77de76b08fcccb3f
Luiz Martino <lhmartino@gmail.com>
2016-08-29 18:31:25 -0300

    Testes de QR Code e Codigo de barras

--- Tests/Form1.cs
+++ Tests/Form1.cs
@@ -512,11 +512,11 @@ namespace Tests
 
             //'-----> Seleccione a posição de impressão de caracteres HRI
             //MSComm1.Output = Chr$(&H1D) + "H" + Chr$(2)
-            printer.sendCommand(new int[] { 29, 72, 1 });
+            printer.sendCommand(new int[] { 29, 72, 0 });
 
             //'-----> Fonte dos Caracteres
             //MSComm1.Output = Chr$(&H1D) + "f" + Chr$(48)
-            //printer.sendCommand(new int[] { 29, 102, 1 });
+            printer.sendCommand(new int[] { 29, 102, 1 });
 
             //'-----> Definir a altura do código de barras
             //MSComm1.Output = Chr$(&H1D) + "h" + Chr$(50)
@@ -525,11 +525,11 @@ namespace Tests
             //'-----> Imprimir codito de barras com modelo correspondente
             //MSComm1.Output = Chr$(&H1D) + "k" + Chr$(73) + Chr$(50) + Chr$(123) + Chr$(66)
             //printer.sendCommand(new int[] { 29, 107, 73, 46, 123, 67 });
-            printer.sendCommand(Convert.ToChar(29).ToString() +
-                                 Convert.ToChar(107).ToString() +
-                                 Convert.ToChar(73).ToString() +
-                                 Convert.ToChar(13).ToString() +
-                                 "{C1234567890123");
+            //printer.sendCommand(Convert.ToChar(29).ToString() +
+            //                     Convert.ToChar(107).ToString() +
+            //                     Convert.ToChar(73).ToString() +
+            //                     Convert.ToChar(13).ToString() +
+            //                     "{C1234567890123");
             
 
             printer.sendCommand(new int[] { 10 });
@@ -537,7 +537,7 @@ namespace Tests
                                  
 
             //MSComm1.Output = "123456789012345678901234567890123456789012345678" + Chr$(&HA)
-            //printer.sendCommand("12345678901234567890123456789012345678901234");
+            printer.sendCommand("35160808723218000186599000048110005992368575");
 
             //'-----> Deixa o texto em negrito: 1 para negrito e 0 para ficar sem negrito
 
@@ -587,6 +587,219 @@ namespace Tests
 
             printer.FechaPorta();
 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //CodigoQrCodeSAT(@"35150909165024000175590000193130072726117830|20150924062259|50.00||hdMEPiER6rjZKyKA+4+voi1nncxsAGFbYsEEqnh04SbvUEI/haUF4GUBPxT6Q2Uhf9f8QYgxiwxWo3GxRrvj4WnNeTYgAqUAYmOANPItNkOw0CppmZ4R8i1ZOlnftVhksCM0zrl4RiKgoazbN44hUu2nQf0W/JLvFXzXu12JlcSThNtmyJ6m9WBsMc/sf9BE14HDoXMyKRIQYt5TkEjilHH9Ffa0saRyUIp+Fji89/Moq8YCCFC+qC44XGxsvNCeeHUNOc1LgPP0DbU1miwpVnrBlEl87RU8Iy0r8fN/fNhbcStkwfTEvhYvZz42nEKHrmGTpGZYkHuTFCNZPq7aCA==");
+            //CodigoDeBarrasSAT("35150909165024000175590000193130072726117830");
+
+
+            printer.ControleAutomaticoDePorta = false;
+
+
+            printer.AbrePorta();
+
+            initPrinter();
+            CodigoDeBarrasSAT2("35150909165024000175590000193130072726117830");
+            acionarGuilhotina();
+
+            printer.FechaPorta();
+
+        }
+
+        private void CodigoQrCodeSAT(string pQrCode)
+        {
+            try
+            {
+                int pH, pL, cn, fn;                
+
+                //'-----> Center n = 0 Esquerda - n = 1 meio = n = 3 direita
+                //MSComm1.Output = Chr$(&H1B) + Chr$(&H61) + Chr$(1) + Chr$(&HA)
+                printer.sendCommand(new int[] { 27, 0x61, 1, 10 });
+
+                //'------> Define tamanho de cada ponto, no caso 3 dots
+                //MSComm1.Output = Chr$(29) + Chr$(40) + Chr$(107) + Chr$(3) + Chr$(0) + Chr$(49) + Chr$(67) + Chr$(5)
+                printer.sendCommand(new int[] { 29, 40, 107, 3, 0, 49, 67, 5 });
+
+
+                //'------> Define nível de redundância
+                //MSComm1.Output = Chr$(29) + Chr$(40) + Chr$(107) + Chr$(3) + Chr$(0) + Chr$(49) + Chr$(69) + Chr$(48)
+                printer.sendCommand(new int[] { 29, 40, 107, 3, 0, 49, 69, 48 });
+
+
+                ////'------> Inicia armazenamento dos dados do código
+                ////MSComm1.Output = Chr$(29) + Chr$(40) + Chr$(107)
+                //sendCommand(new int[] { 29, 40, 107 });
+                //Log("QrCode Length: " + pQrCode.Length.ToString());
+
+                ////'-----> 'Informando o tamanho do dados do QR Code, acrescido de 3 unidades. Neste exemplo, o código tem 508 bytes, portanto o tamanho será 511]
+                ////'Comando GS + ( + K )) é maior que 256, então tem que dividir utilizando os bytes pH e pL  511 divido por 256 = 1,996094 e sobra (511 - 256) = 255, então fica assim:
+                ////MSComm1.Output = Chr$(91) + Chr$(1) + Chr$(49) + Chr$(80) + Chr$(48)
+                //pL = (pQrCode.Length + 3) % 256; //= 91;
+                //pH = 1;
+                //cn = 49;
+                //fn = 80;
+
+                //Log("pL: " + pL.ToString());
+
+                ////pL = 158;
+                ////pH = 1;
+                ////cn = 49;
+                ////fn = 80;
+
+                //sendCommand(new int[] { pL, pH, cn, fn, 48 });
+
+                ////'-----> Dados do Codigo
+                ////MSComm1.Output = QrCode
+
+                Encoding m_encoding = Encoding.GetEncoding("iso-8859-1");
+                porta.Encoding = m_encoding;
+                string QRdata = pQrCode;
+                int store_len = QRdata.Length + 3; // 414
+                byte store_pL = (byte)(store_len % 256); // 158
+                byte store_pH = (byte)(store_len / 256); // 1
+                byte[] data = new byte[] { 29, 40, 107, store_pL, store_pH, 49, 80, 48 }; //FUNCTION 180
+                string txt = m_encoding.GetString(data);
+                byte[] invalidData = m_encoding.GetBytes(txt);
+
+                int[] bytesAsInts = invalidData.Select(x => (int)x).ToArray();
+                printer.sendCommand(bytesAsInts);
+
+                byte[] bytes = m_encoding.GetBytes(QRdata);
+                QRdata = m_encoding.GetString(bytes);
+
+                //Print(QRdata);
+                printer.sendCommand(bytes.Select(x => (int)x).ToArray());
+                //PrintLine("");
+
+
+                //'------> Imprime código e finaliza processo
+                //MSComm1.Output = Chr$(29) + Chr$(40) + Chr$(107) + Chr$(3) + Chr$(0) + Chr$(49) + Chr$(81) + Chr$(48) + Chr$(12)
+                printer.sendCommand(new int[] { 29, 40, 107, 3, 0, 49, 81, 48, 12 });
+
+                //'-----> Impressão e alimentação de n linhas
+                //Me.MSComm1.Output = Chr$(27) + Chr$(100) + Chr$(15)
+                printer.sendCommand(new int[] { 27, 100, 2 });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+
+        /// <summary>
+        /// Imprime duas linhas de código de barras informado
+        /// </summary>
+        /// <param name="pCodBarras"></param>
+        private void CodigoDeBarrasSAT(string pCodBarras)
+        {
+
+            //Example data for printing "No. 123456"
+            //GS k 73 10 123 66 78 111 46 123 67 12 34 56
+            //sendCommand(new int[] {29, 107, 73, 10, 123, 66, 78, 111, 46, 123, 67, 12, 34, 56});
+
+            /*
+                In this example, the printer first prints "No." using Code B, then prints the following numbers using Code C.
+             * Heres's what I know about this example - 
+             * the GS stands for "Group Separation" and is coded as Chr(29) and the "k" (Chr(107)) combined with GS is 
+             * supposed to tell the printer it will be printing a bar code.  "73" is for CODE 128 and the 10 is for the 
+             * number of characters in the bar code (No. 123456) = 10.  Then "123 66" tells the printer to print using 
+             * CODE B the next three characters (N = 78, o = 111 and . = 46).  Next "123 67" tells the printer to print 
+             * using CODE C the numbers 123456.
+             */
+
+            printer.sendCommand(new int[] { 29, 104, 81 }); //Altura. Padrão 162            
+            printer.sendCommand(new int[] { 29, 119, 2 }); // "grossura" das barras
+            printer.sendCommand(new int[] { 29, 107, 73, 24, 123, 66 });   // Code 128, padrão "B" (66)
+            //PrintLine("3516080872321800018659");
+            printer.PrintLine(pCodBarras.Substring(0, 22));
+
+            printer.sendCommand(new int[] { 29, 104, 81 }); //Altura. Padrão 162           
+            printer.sendCommand(new int[] { 29, 119, 2 }); // "grossura" das barras
+            printer.sendCommand(new int[] { 29, 107, 73, 24, 123, 66 }); // Code 128, padrão "B" (66)
+            //PrintLine("9000048110005992368575");                        
+            printer.PrintLine(pCodBarras.Substring(22, 22));
+
+            //sendCommand(new int[] { 0 });
+
+        }
+
+        private void CodigoDeBarrasSAT2(string pCodBarras)
+        {
+            
+            Encoding m_encoding = Encoding.GetEncoding("iso-8859-1");
+            porta.Encoding = m_encoding;
+
+            byte[] invalidData = m_encoding.GetBytes(pCodBarras);
+            int[] bytesAsInts = invalidData.Select(x => (int)x).ToArray();
+
+            
+            string vCod = m_encoding.GetString(invalidData);
+
+            MessageBox.Show(bytesAsInts.Length.ToString());
+            MessageBox.Show(vCod);
+
+            //Example data for printing "No. 123456"
+            //GS k 73 10 123 66 78 111 46 123 67 12 34 56
+            //sendCommand(new int[] {29, 107, 73, 10, 123, 66, 78, 111, 46, 123, 67, 12, 34, 56});
+
+            /*
+                In this example, the printer first prints "No." using Code B, then prints the following numbers using Code C.
+             * Heres's what I know about this example - 
+             * the GS stands for "Group Separation" and is coded as Chr(29) and the "k" (Chr(107)) combined with GS is 
+             * supposed to tell the printer it will be printing a bar code.  "73" is for CODE 128 and the 10 is for the 
+             * number of characters in the bar code (No. 123456) = 10.  Then "123 66" tells the printer to print using 
+             * CODE B the next three characters (N = 78, o = 111 and . = 46).  Next "123 67" tells the printer to print 
+             * using CODE C the numbers 123456.
+             */
+
+            printer.sendCommand(new int[] { 29, 104, 81 }); //Altura. Padrão 162            
+            printer.sendCommand(new int[] { 29, 119, 1 }); // "grossura" das barras
+            printer.sendCommand(new int[] { 29, 107, 73, bytesAsInts.Length+2, 123, 65 });   // Code 128, padrão "B" (66)
+            //PrintLine("3516080872321800018659");
+            printer.PrintLine(vCod);
+
+           
+
+            //sendCommand(new int[] { 0 });
+
+        }
+
+        private void initPrinter()
+        {
+            // ESC @ --> Inicializa a impressora            
+            printer.sendCommand(new int[] { 27, 64 });
+
+            // ESC = --> Habilita a impressora
+            printer.sendCommand(new int[] { 27, 61, 1 });
+
+            //// ESC ! n 
+            //sendCommand(new int[] { 27, 33, 0 });
+
+            // ESC M n 
+            printer.sendCommand(new int[] { 27, 77, 0 });
+
+            //// ESC ? n 
+            //sendCommand(new int[] { 27, 63, 0 });
+
+            //// ESC - n 
+            //sendCommand(new int[] { 27, 45, 0 });
+
+            // ESC t n 
+            printer.sendCommand(new int[] { 27, 116, 3 });
+
+            // ESC R n 
+            printer.sendCommand(new int[] { 27, 82, 12 });
+
+            // ESC 3 n --> Definição do espaçamento entre linhas            
+            printer.sendCommand(new int[] { 27, 51, 25 }); 
+        }
+        private void acionarGuilhotina()
+        {
+            printer.sendCommand(new int[] { 29, 86, 66, 0 });
         }
     }
 }
